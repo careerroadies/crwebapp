55478acfe963163a29c570ebe88eb9fdc6bd03ff
careerroadies <deepakb.net@gmail.com>
2017-01-11 19:24:17 +0530

    notes UI

--- crwebapp/components/notes/controllers/notes.controller.js
+++ crwebapp/components/notes/controllers/notes.controller.js
@@ -1,1 +1,11 @@
-﻿
+﻿crApp.controller("notesController", ['$scope', notesController]);
+
+function notesController($scope)
+{
+    $scope.profileId = "";
+    $scope.modalShown = false;
+    $scope.toggleModal = function (profileid) {
+        $scope.modalShown = !$scope.modalShown;
+        $scope.profileId = 1;
+    };
+}
